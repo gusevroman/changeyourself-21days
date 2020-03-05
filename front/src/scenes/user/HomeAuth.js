e04104c01f7a6b801a614d01c6e48a3867cadccd
artem geroyan <geroyan.artem@mail.ru>
2020-03-05 14:12:26 +0300

    feat: add account carousel

--- front/src/scenes/user/HomeAuth.js
+++ front/src/scenes/user/HomeAuth.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+import CarouselAccount from "./Carousel/CarouselAccount";
+import InfoButton from "./infoButton/InfoButton";
+
 
 class Home extends React.Component {
 
+
+
   render() {
-    const { isLoggined } = this.props
+    const { isLoggined } = this.props;
     return (
       <>
+
          <h2  style={{ textAlign: "center", margin: "30px 0px" }}>Welcome {isLoggined}</h2>
+        < InfoButton />
+        < CarouselAccount />
       </>
     );
   }
