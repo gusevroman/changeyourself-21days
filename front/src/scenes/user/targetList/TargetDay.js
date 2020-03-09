8a5ef52a4f94cb22aeec27fc02e12a6fd9d70fc6
Alexander Polubnev <alexpolubnev8@gmail.com>
2020-03-09 20:06:10 +0300

    added rus date

--- front/src/scenes/user/targetList/TargetDay.js
+++ front/src/scenes/user/targetList/TargetDay.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import moment from 'moment';
-
-
 export default class TargetDay extends Component {
-
+    
 
   constructor(props) {
     super(props)
@@ -17,11 +15,14 @@ export default class TargetDay extends Component {
   componentDidMount(){
     let { date, counter } = this.props
     date = date.setDate(date.getDate() + counter);
-    const dayDate = new Date(date) + '';
-
-    this.setState({date: moment(dayDate).format('DD MMMM YYYY')});
+    moment.lang('ru', {
+      months : {
+        format: 'января_февраля_марта_апреля_мая_июня_июля_августа_сентября_октября_ноября_декабря'.split('_'),
+        standalone: 'январь_февраль_март_апрель_май_июнь_июль_август_сентябрь_октябрь_ноябрь_декабрь'.split('_')
+      }
+    });
+    this.setState({date: moment(date).format('DD MMMM YYYY')});
   }
-
 
   async handleStatus() {
     await fetch(`http://localhost:5000/targetlist/status`, {
@@ -48,11 +49,11 @@ export default class TargetDay extends Component {
       <>
         <div className="target_spoiler">
           <div className="task">
-            <h2 className="target_spoiler_title" onClick={this.handleSpoiler.bind(this)}>{day.title}</h2>
+            <h2 className="target_spoiler_title" onClick={this.handleSpoiler.bind(this)}>{this.state.date}</h2>
             <input class="tgl tgl-flip" id={this.props.day._id} type="checkbox"
               onClick={this.handleStatus.bind(this)}
             />
-            <label class="tgl-btn" data-tg-off="Nope" data-tg-on="Done!" for={this.props.day._id}></label>
+          <label class="tgl-btn" data-tg-off="Nope" data-tg-on="Done!" for={this.props.day._id}></label>
           </div>
           <div className="spoiler" style={{ display: this.state.display }}>
             <h2 align="center">{day.description}</h2>
