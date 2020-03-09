9734869b58d5bc3b3506d675f73ff937b14fc864
gusevroman <949397@mail.ru>
2020-03-09 15:57:41 +0300

    add input into profile

--- front/src/components/Routes.js
+++ front/src/components/Routes.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 import Home from '../scenes/home/Home';
-import Profile from '../scenes/user/profile/profile';
+import Profile from '../scenes/user/profile/Profile';
 import SelectTarget from "../scenes/user/selectTarget/selectTarget";
 import TargetList from '../scenes/user/targetList/TargetList';
 import MethodInfo from '../scenes/home/Methods/methodInfo/MethodInfo';
@@ -11,25 +11,25 @@ import MethodInfo from '../scenes/home/Methods/methodInfo/MethodInfo';
 
 function Routes() {
 
-    return (
-        <Switch>
-            <Route exact path="/">
-                <Home/>
-            </Route>
-            <Route exact path="/user/profile">
-                <Profile />
-            </Route>
-            <Route exact path="/newTarget">
-                <SelectTarget />
-            </Route>
-            <Route path="/target/:id">
-                <TargetList />
-            </Route>
-            <Route path="/method/:id">
-                <MethodInfo />
-            </Route>
-        </Switch>
-    )
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/user/profile">
+        <Profile />
+      </Route>
+      <Route exact path="/newTarget">
+        <SelectTarget />
+      </Route>
+      <Route path="/target/:id">
+        <TargetList />
+      </Route>
+      <Route path="/method/:id">
+        <MethodInfo />
+      </Route>
+    </Switch>
+  )
 }
 
 export default connect()(Routes)
