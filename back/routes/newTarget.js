967782099a95320e195d1d47d89b823742c9969f
Ilya <freedrex@gmail.com>
2020-03-10 20:22:59 +0300

    add target to my targets

--- back/routes/newTarget.js
+++ back/routes/newTarget.js
@@ -3,9 +3,26 @@ const Target = require('../models/target');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const allTarget = await Target.find({});
-    return res.json(allTarget)
+  const allTarget = await Target.find({});
+  return res.json(allTarget)
 });
 
+router.post('/add', async (req, res) => {
+  const { userId, method } = req.body
+  console.log(userId);
+  console.log(method);
+  const newTarget = await new Target({
+    category: method.category,
+    status: "active",
+    title: method.title,
+    description: method.description,
+    tag: method.tag,
+    startDate: new Date(),
+    endDate: new Date(),
+    author: userId,
+    method: method.method,
+  })
+  await newTarget.save()
+});
 
 module.exports = router;
