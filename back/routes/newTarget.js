5b18748bad38aae57889ad5ccc038e615357bf02
artem geroyan <geroyan.artem@mail.ru>
2020-03-11 16:29:48 +0300

    feat: add email sender

--- back/routes/newTarget.js
+++ back/routes/newTarget.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const Target = require('../models/target');
+const User = require('../models/user');
 const router = express.Router();
+const nodemailer = require("nodemailer");
+
+
+
 
 router.get('/', async (req, res) => {
   const allTarget = await Target.find({});
@@ -9,8 +14,10 @@ router.get('/', async (req, res) => {
 
 router.post('/add', async (req, res) => {
   const { userId, method } = req.body
-  console.log(userId);
-  console.log(method);
+  let ourUser = await User.findById(userId);
+  console.log('НАШ ЮЗЕР', ourUser.email);
+
+
   const newTarget = await new Target({
     category: method.category,
     status: "active",
@@ -21,8 +28,45 @@ router.post('/add', async (req, res) => {
     endDate: new Date(),
     author: userId,
     method: method.method,
-  })
-  await newTarget.save()
+  });
+  await newTarget.save();
+
+  let allTargetsUser = await Target.find({
+    author: userId
+  });
+
+  let allTitleTarget = allTargetsUser.filter(elem => elem.title !== undefined).map(elem => elem.title).join(', ');
+  console.log('YFI VTN{JL', method);
+
+
+
+  async function main() {
+    let transporter = nodemailer.createTransport({
+      host: "smtp.yandex.ru",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: "days21go@yandex.ru", // generated ethereal user
+        pass: '21DAYS' // generated ethereal password
+      }
+    });
+
+    let info = await transporter.sendMail({
+      from: '"НУ ЗДАРОВА 👻" <days21go@yandex.ru>', // sender address
+      to: 'geroyan.artem@mail.ru', // list of receivers
+      subject: "Вы записаны! ", // Subject line
+      text: "Информация о записе", // plain text body
+      html: `<b>Здравствуйте! Вы выбрали ${method.method.title}. Методика была добавлена.</b>
+                                <p>Список методик: ${allTitleTarget} </p>`
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+  }
+
+  main().catch(console.error);
+
 });
 
 module.exports = router;
--- front/src/scenes/home/Methods/methodInfo/ AddMethodToTarget.js
+++ front/src/scenes/home/Methods/methodInfo/ AddMethodToTarget.js
@@ -5,8 +5,9 @@ import { withRouter } from "react-router-dom";
 class AddMethodToTarget extends Component {
 
   async addTarget(){
-    const method = this.props.method
-    const userId = this.props.userId    
+    const method = this.props.method;
+    const userId = this.props.userId;
+    console.log('>>>>>>>', this.props.userEmail);
     await fetch('http://localhost:5000/newTarget/add', {
       method: "POST",
       headers: {
@@ -27,7 +28,7 @@ class AddMethodToTarget extends Component {
 }
 const mapStateToProps = state => ({
   isLoggined: state.isLoggined,
-  userId: state.userId,
+  userId: state.userId
 });
 
 const mapDispatchToProps = dispatch => ({});
