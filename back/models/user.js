e4316e43805cb21fa23faaf115a8dd1fa7815ff6
artem geroyan <geroyan.artem@mail.ru>
2020-03-12 14:29:28 +0300

    fix: some fixes

--- back/models/user.js
+++ back/models/user.js
@@ -9,6 +9,8 @@ const userSchema = new mongoose.Schema({
     tel: String,
     instagram: String,
     profileImg: String,
+    checkEmail: {type: Boolean, default: false},
+    style: {type: String, default: 'Light'},
 });
 
 module.exports = mongoose.model('User', userSchema);
